e29a2c3be4d274c81a49bc593e033e57c77a4280
azam <azambagwan567@gmail.com>
2023-02-06 12:15:04 +0530

    Linked List Added

---
+++ src/com/collection/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+	List  list = new java.util.LinkedList();
+	list.add(37);
+	list.add(3);
+	list.add(35);
+	list.add(456);
+	list.add(3457);
+	System.out.println(list);
+	 
+	}
+
+}
